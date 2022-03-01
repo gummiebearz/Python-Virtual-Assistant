e593848609492be855815440ad4bd7aaf4fedae4
Gumball09 <vivianpham0904@gmail.com>
2022-02-28 18:52:06 -0800

    Bot Application User Interface

--- bot_app.py
+++ bot_app.py
@@ -6,23 +6,8 @@ class BotApp():
         self.__bot = Bot()
     
     def run(self):
-        while True:
-            cmd = self.__bot.take_command()
-            print(f"* Command: {cmd}")
-
-            if 'exit' in cmd or 'goodbye' in cmd:
-                self.__bot.say('I wish to see you again!')
-                print("BOT SHUTTING DOWN...")
-                break
-            elif cmd == '':
-                self.__bot.say('Sorry, please say the command again! It starts with "Hey Alexa", followed by the chosen function')
-                print("-> Missing required command...")
-            else:
-                self.__bot.process_command(cmd)
-    
-    def __str__(self):
-        return "This is a simplified bot application created by Gumball!"
+        self.__bot.run()
 
 if __name__ == '__main__':
     app = BotApp()
-    app.run()
+    app.run

87dcfc76ce6e29b339360c9a2c28b187f9925250
Gumball09 <vivianpham0904@gmail.com>
2022-03-01 14:23:25 -0800

    Add __str__ description

--- bot_app.py
+++ bot_app.py
@@ -7,7 +7,11 @@ class BotApp():
     
     def run(self):
         self.__bot.run()
+    
+    def __str__(self):
+        return f"Hello. This is the user interface for using {self.__bot.bot_name.upper()}, the best virtual assistant."
 
 if __name__ == '__main__':
     app = BotApp()
     app.run()
+    # print(app)

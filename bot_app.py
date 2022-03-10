55af9922620e2d24b0ebaf2e787d5ce74b9baddc
Gumball09 <vivianpham0904@gmail.com>
2022-03-09 20:14:38 -0800

    Move run() logic to bot application

--- bot_app.py
+++ bot_app.py
@@ -6,7 +6,26 @@ class BotApp():
         self.__bot = Bot()
     
     def run(self):
-        self.__bot.run()
+        while True:
+            try:
+                cmd = self.__bot.take_command()
+                print(f"* Command: {cmd if cmd != '' and cmd != None else 'Unknown'}")
+
+                if cmd in ['exit', 'goodbye', 'shutdown', 'shut down']:
+                    self.__bot.say('I wish to see you again!')
+                    print("BOT SHUTTING DOWN...")
+                    break
+                elif cmd == '':
+                    self.__bot.say(f'Sorry, please say the command again! It starts with "Hey {self.__bot.bot_name.upper()}", followed by your prefered utility. For a list of utilities, please say "Hey {self.__bot.bot_name.upper()}", see utilities"')
+                    print("-> Missing required command...")
+                else:
+                    self.__bot.process_command(cmd)
+
+                print()
+            except ValueError:
+                self.__bot.say('Sorry, there was an error getting audio input. Shutting down...')
+                print("Failed to get audio input. BOT SHUT DOWN!")
+                break
     
     def __str__(self):
         return f"Hello. This is the user interface for using {self.__bot.bot_name.upper()}, the best virtual assistant."

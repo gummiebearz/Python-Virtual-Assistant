dbac46381fe83ad7ca7cdb8f6bab5d6394131633
Gumball09 <vivianpham0904@gmail.com>
2022-03-09 20:49:08 -0800

    Move run(), process_command(), take_command() to user interface

--- bot_app.py
+++ bot_app.py
@@ -4,24 +4,60 @@ from bot import Bot
 class BotApp():
     def __init__(self):
         self.__bot = Bot()
-    
+
+    # Take user's command
+    def take_command(self):
+        try:
+            cmd = self.__bot.get_audio_input()
+
+            if f"hey {self.__bot.bot_name}" in cmd:
+                cmd = cmd.replace(f"hey {self.__bot.bot_name}", '').strip()
+                return cmd
+            return '' 
+        except:
+            raise ValueError()
+
+    # Process user's command
+    def process_command(self, cmd):
+        if 'see utilities' in cmd:
+            self.__bot.get_utilities()
+        elif 'play' in cmd and 'music' in cmd:
+            self.__bot.play_music()            
+        elif 'weather' in cmd:
+            self.__bot.get_weather(cmd)
+        elif 'joke' in cmd or 'jokes' in cmd:
+            self.__bot.get_joke()
+        elif 'time' in cmd:
+            self.__bot.get_time()
+        elif 'today' in cmd and 'date' in cmd:
+            self.__bot.get_date()
+        elif 'send' in cmd and 'email' in cmd:
+            self.__bot.send_email()
+        elif 'add' in cmd and 'email' in cmd:
+            self.__bot.add_email_contact()
+        else:
+            self.__bot.say(f'Sorry, please say the command again! It starts with "Hey {self.bot_name}", followed by your prefered utility. For a list of utilities, please say "Hey {self.bot_name} see utilities"')
+            
     def run(self):
         while True:
             try:
-                cmd = self.__bot.take_command()
+                cmd = self.take_command()
                 print(f"* Command: {cmd if cmd != '' and cmd != None else 'Unknown'}")
 
                 if cmd in ['exit', 'goodbye', 'shutdown', 'shut down']:
                     self.__bot.say('I wish to see you again!')
                     print("BOT SHUTTING DOWN...")
                     break
+
                 elif cmd == '':
                     self.__bot.say(f'Sorry, please say the command again! It starts with "Hey {self.__bot.bot_name.upper()}", followed by your prefered utility. For a list of utilities, please say "Hey {self.__bot.bot_name.upper()}", see utilities"')
                     print("-> Missing required command...")
+
                 else:
-                    self.__bot.process_command(cmd)
+                    self.process_command(cmd)
 
                 print()
+
             except ValueError:
                 self.__bot.say('Sorry, there was an error getting audio input. Shutting down...')
                 print("Failed to get audio input. BOT SHUT DOWN!")
@@ -32,5 +68,5 @@ class BotApp():
 
 if __name__ == '__main__':
     app = BotApp()
-    app.run()
-    # print(app)
+    # app.run()
+    print(app)

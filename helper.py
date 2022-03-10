47fb683614a896b676516df30921264f99820de9
Gumball09 <vivianpham0904@gmail.com>
2022-03-09 20:03:51 -0800

    Update except error handling

--- helper.py
+++ helper.py
@@ -15,7 +15,7 @@ try:
             name, email = line
             emails[name.lower()] = email.lower()
 except:
-    emails = None
+    emails = {}
     print(f"*** ERROR: Could not open file {configs['FILE_EMAILS']}")
 
 ### LOAD UTILITIES FROM utilities.txt
@@ -23,5 +23,5 @@ try:
     with open(configs["FILE_UTILITIES"], "r") as file:
         utilities = file.read().replace("\n", ", ")
 except:
-    utilities = None
+    utilities = {}
     print(f"*** ERROR: Could not open file {configs['FILE_UTILITIES']}")

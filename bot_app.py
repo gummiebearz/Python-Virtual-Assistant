9d5be94aaf919bc9ac1d5eef03cd95eab0be0c10
Gumball09 <vivianpham0904@gmail.com>
2022-03-10 17:06:52 -0800

    Update exception handing for audio input

--- bot_app.py
+++ bot_app.py
@@ -1,38 +1,47 @@
 ### USER INTERFACE FOR BOT APPLICATION ###
+import speech_recognition as sr
 from bot import Bot
 
 class BotApp():
     def __init__(self):
         self.__bot = Bot()
 
+    def __str__(self):
+        return f"Hello. This is the user interface for using {self.__bot.bot_name.upper()}, the best virtual assistant."
+
     # Take user's command
     def take_command(self):
-        try:
-            cmd = self.__bot.get_audio_input()
+        cmd = self.__bot.get_audio_input()
 
-            if f"hey {self.__bot.bot_name}" in cmd:
-                cmd = cmd.replace(f"hey {self.__bot.bot_name}", '').strip()
-                return cmd
-            return '' 
-        except:
-            raise ValueError()
+        if f"hey {self.__bot.bot_name}" in cmd:
+            cmd = cmd.replace(f"hey {self.__bot.bot_name}", '').strip()
+            return cmd
+        return '' 
 
     # Process user's command
     def process_command(self, cmd):
+        # show utilties
         if 'see utilities' in cmd:
             self.__bot.get_utilities()
+        # play music
         elif 'play' in cmd and 'music' in cmd:
             self.__bot.play_music()            
+        # get weather data
         elif 'weather' in cmd:
             self.__bot.get_weather(cmd)
+        # get joke
         elif 'joke' in cmd or 'jokes' in cmd:
             self.__bot.get_joke()
-        elif 'time' in cmd:
+        # get current time
+        elif 'what' in cmd and 'time' in cmd:
             self.__bot.get_time()
+        # get current date
         elif 'today' in cmd and 'date' in cmd:
             self.__bot.get_date()
+        # send email
         elif 'send' in cmd and 'email' in cmd:
             self.__bot.send_email()
+        # create new email contact
         elif 'add' in cmd and 'email' in cmd:
             self.__bot.add_email_contact()
         else:
@@ -61,15 +70,13 @@ class BotApp():
 
                 print()
 
-            except ValueError:
-                self.__bot.say('Sorry, there was an error getting audio input. Shutting down...')
-                print("Failed to get audio input. BOT SHUT DOWN!")
+            except sr.UnknownValueError as err_msg:
+                print(err_msg)
+                self.__bot.say("Sorry, there was an error getting the audio input. Shutting down...")
 
                 self.__bot.shutdown_tasks()
                 break
     
-    def __str__(self):
-        return f"Hello. This is the user interface for using {self.__bot.bot_name.upper()}, the best virtual assistant."
 
 if __name__ == '__main__':
     app = BotApp()
